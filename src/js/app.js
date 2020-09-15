7666afb6393d4d6c29b7773a15c99e7d530a89b9
feyaq13 <feyaq13@gmail.com>
2020-09-15 11:49:53 +0300

    выносит интерфейс из объекта в класс

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
-import { gui } from './gui';
+import { GUI } from './gui';
 import { Game } from './game';
 import { SessionStorageGameHistoryStorage } from './history-storage/session-storage-game-history-storage';
 
 const storage = new SessionStorageGameHistoryStorage();
+const gui = new GUI();
 
 const ticTacToe = new Game({ isAgainstComputer: true, gui, storage });
 ticTacToe.processNextTurn();
--- src/js/gui.js
+++ src/js/gui.js
@@ -1,26 +1,35 @@
-export const gui = {
-  _onResetCb: null,
-  _btnNewGame: document.getElementsByClassName('btn-new-game')[0],
+export class GUI {
+  constructor() {
+    this._onResetCb = null;
+    this._btnNewGame = document.getElementsByClassName('btn-new-game')[0];
+  }
+
   displayPlayedGames(number) {
     document.getElementsByClassName('statics__number-of-games-played')[0].textContent = number;
-  },
+  }
+
   markCell(cellInd, player) {
     document.getElementsByClassName('cell')[cellInd].textContent = player.symbol;
-  },
+  }
+
   showNewGameButton() {
     this._btnNewGame.hidden = false;
     this._btnNewGame.addEventListener('click', this._onResetCb);
-  },
+  }
+
   hideNewGameButton() {
     this._btnNewGame.hidden = true;
     this._btnNewGame.removeEventListener('click', this._onResetCb);
-  },
+  }
+
   onResetClicked(cb) {
     this._onResetCb = cb;
-  },
+  }
+
   clearCells() {
     for (let i = 0; i < 9; i++) {
       document.getElementsByClassName('cell')[i].textContent = null;
     }
-  },
-};
+  }
+
+}
